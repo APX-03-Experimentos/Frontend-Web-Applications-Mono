88f84db2eb04e74b9af28d58c56e56cb0dea4a4f
sebastianrealcalderon <u20221d964@upc.edu.pe>
2025-11-12 20:35:40 -0500

    fix: update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverBaseUrl: "http://https://backend-web-services-1.onrender.com/api/v1",
+  serverBaseUrl: "https://backend-web-services-1.onrender.com/api/v1",
   websocketBaseUrl: 'ws://https://backend-web-services-1.onrender.com/ws-notifications',
   authenticationEndpointPath: '/authentication',
   usersEndpointPath: '/users',
