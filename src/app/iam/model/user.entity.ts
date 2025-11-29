570c6c79f98b189dbf819560892a2a8f42c9062d
sebastianrealcalderon <u20221d964@upc.edu.pe>
2025-11-29 02:51:50 -0500

    feat: redesign for assignment pages

--- src/app/iam/model/user.entity.ts
+++ src/app/iam/model/user.entity.ts
@@ -1,13 +1,13 @@
 export type Role = 'ROLE_STUDENT' | 'ROLE_TEACHER';
 export class User {
   id: number;
-  userName: string;
+  username: string;
   token: string;
   roles: Role[];
 
-  constructor(user:{id?: number, userName?: string, token?: string, roles?: Role[]}) {
+  constructor(user:{id?: number, username?: string, token?: string, roles?: Role[]}) {
     this.id = user.id || 0;
-    this.userName = user.userName || '';
+    this.username = user.username || '';
     this.token = user.token || '';
     this.roles = user.roles || [];
   }
